79a866ab46677e1b3c7284872151f14e6fe92aff
cebrailakar <akarcebrail68@gmail.com>
2025-11-03 16:45:16 +0000

    public klasörünü static yapmak

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,5 +3,8 @@ import "$lib/locale/load";
 import { waitLocale } from 'svelte-i18n'
 
 export const load: LayoutLoad = async () => {
-    return waitLocale()
+    await waitLocale()
+    return {
+        localeReady: true
+    }
 };
