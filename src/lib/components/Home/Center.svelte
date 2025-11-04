2baab3f1ceeb7c9e9055c5377e03da62c9e9a5a4
cebrailakar <akarcebrail68@gmail.com>
2025-11-04 11:40:15 +0000

    Tema

--- src/lib/components/Home/Center.svelte
+++ src/lib/components/Home/Center.svelte
@@ -1,12 +1,12 @@
 <script>
     import { _ } from 'svelte-i18n';
 </script>
-<div class="h-screen flex flex-col items-center justify-center space-y-4">
+<div class="h-screen flex flex-col items-center justify-center space-y-4 perspective-distant">
 <!-- Title -->
-<h1 class="text-6xl font-bold text-gray-800 -mt-12 mb-30">{$_("home.title")}</h1>
+<h1 class="text-6xl font-bold text-gray-800 -mt-12 mb-30 rotate-x-51 rotate-z-43 transform-3d">{$_("home.title").toUpperCase()}</h1>
 
 <!-- Play Button -->
-<button class="bg-blue-500 text-white px-6 py-3 w-45">
+<button class="bg-custom-1 text-white px-6 py-3 w-45 hover:bg-white">
 {$_("home.play")}
 </button>
 
